28eeb5fe261341ddbf8a97900106824eee64c0ee
dryflowery <sechane12@naver.com>
2024-11-13 12:34:51 +0900

    asdf

--- MoleuGo/src/component/LoginPopup.svelte
+++ MoleuGo/src/component/LoginPopup.svelte
@@ -30,7 +30,7 @@
             <button id="login-btn">로그인</button>
             
             <div id="link-container">
-                <a on:click={()=> push('/#/findpassword')}>비밀번호 찾기</a> | <a on:click={()=> push('/#/signup')}>회원가입</a>
+                <a on:click={()=> push('/findpassword')}>비밀번호 찾기</a> | <a on:click={()=> push('/signup')}>회원가입</a>
             </div>
             
             <div class="divider"></div>
